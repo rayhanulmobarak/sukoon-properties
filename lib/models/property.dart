eabd8c58d2d70b0dbeed8ddc0c68307a7eddf45a
rayhanulmobarak <mrayhanul@gmail.com>
2026-08-01 20:23:15 +0300

    Add Property class with necessary fields

---
+++ lib/models/property.dart
@@ -0,0 +1,29 @@
+class Property {
+
+  final String id;
+  final String name;
+  final String location;
+  final String price;
+  final String area;
+  final String type;
+  final String image;
+  final String description;
+  final List<String> facilities;
+
+
+  Property({
+
+    required this.id,
+    required this.name,
+    required this.location,
+    required this.price,
+    required this.area,
+    required this.type,
+    required this.image,
+    required this.description,
+    required this.facilities,
+
+  });
+
+
+}
